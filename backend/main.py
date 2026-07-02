8c674d844d46e03e71d7e60650d30273b2ed42ee
Abdul Rahman Rifayath <abdulrahmanrifayath264@gmail.com>
2026-07-02 12:55:02 +0530

    connecting fast api to my sql
    day2

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,24 @@
 from fastapi import FastAPI
 
+from backend.database.database import Base, engine
+
+import backend.models.user
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
     title="API Optimizer AI",
-    description="AI-powered API Usage Monitor & Optimization Advisor",
-    version="1.0.0"
+    version="1.0.0",
+    description="AI Powered API Usage Monitor"
 )
+
 
 @app.get("/")
 def home():
     return {
         "message": "Welcome to API Optimizer AI 🚀"
     }
+
 
 @app.get("/health")
 def health():
